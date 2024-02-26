bdc330d79b65b91863dddaa37da137faa57709ff
haoptimizervn <nguyen_duy_ha@optimizer.co.jp>
2024-02-27 00:39:21 +0700

    m_1 26/2

---
+++ 1/src/LearnJava/HelloWorld.java
@@ -0,0 +1,7 @@
+package LearnJava;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world with Java");
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Module-code-and-snippets_Java
+<p>List of modules:</p>
+<li>Getting started and learn syntax</li>
