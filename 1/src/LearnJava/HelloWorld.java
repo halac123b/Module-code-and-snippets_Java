13c924ee5a2ce85d543a9c881a79691b5b9ec9b7
halac123b <halac123b@gmail.com>
2024-03-02 19:49:12 +0700

    m_1 2/3

--- 1/src/LearnJava/HelloWorld.java
+++ 1/src/LearnJava/HelloWorld.java
@@ -1,7 +1,15 @@
 package LearnJava;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello world with Java");
+		int a = 5;
+		float f = 10.55f;
+		System.out.printf("There are %d people have %.2f dollars", a, f);
+		System.out.println("Concat string " + a);
+
+		Scanner scanner = new Scanner(System.in);
 	}
 }
