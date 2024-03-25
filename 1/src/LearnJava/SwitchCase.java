21424c95b19a122d67595a638d582c22a9baaab3
halac123b <halac123b@gmail.com>
2024-03-25 21:06:53 +0700

    m_1 25/3

--- 1/src/LearnJava/SwitchCase.java
+++ 1/src/LearnJava/SwitchCase.java
@@ -2,6 +2,19 @@ package LearnJava;
 
 public class SwitchCase {
 	public static void main(String[] args) {
-
+		int a = 5;
+		switch (a) {
+		case 1: {
+			System.out.print("Number 5");
+			break;
+		}
+		case 2: {
+			System.out.print("Number 2");
+			break;
+		}
+		default:
+			System.out.print("Exception case");
+			System.exit(0);
+		}
 	}
 }
