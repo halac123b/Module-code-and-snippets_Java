a9582d9e41314b984abcb5bb4fc918fd03eecb03
halac123b <halac123b@gmail.com>
2024-04-22 02:01:11 +0700

    Add m_2 21/4

---
+++ 2/Ball.java
@@ -0,0 +1,8 @@
+package cannonsimulator;
+
+// awt: Abstract Window Toolkit, lib for GUI
+import java.awt.Color;
+
+public class Ball {
+  private double x, y;
+}
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Module-code-and-snippets_Java
+
 <p>List of modules:</p>
+<ol>
 <li>Getting started and learn syntax</li>
+<li>Canon simulator</li>
+<ol>
