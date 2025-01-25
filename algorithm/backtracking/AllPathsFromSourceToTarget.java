a8c4ea11d28bc336fd3fca92132c7884e7c5a4a1
halac123b <halac123b@gmail.com>
2025-01-25 12:35:10 +0700

    Add Algorithm module

---
+++ algorithm/backtracking/AllPathsFromSourceToTarget.java
@@ -0,0 +1,10 @@
+package algorithm.backtracking;
+
+public class AllPathsFromSourceToTarget {
+    /**
+     * Program description - To find all possible paths from source to destination
+     * <a href="https://en.wikipedia.org/wiki/Shortest_path_problem">Wikipedia</a>
+     *
+     * @author <a href="https://github.com/siddhant2002">Siddhant Swarup Mallick</a>
+     */
+}
